2fceaf203e3c7536e8a8c2cfdc1b7e31599dbe2c
timi0809 <112700699+timi0809@users.noreply.github.com>
2022-09-09 20:50:08 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -15,5 +15,5 @@
 
 # Add a feed source
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
-#echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+echo 'git clone https://github.com/Zxilly/UA2F.git package/UA2F' >>feeds.conf.default
 echo 'src-git small8 https://github.com/kenzok8/small-package' >>feeds.conf.default
